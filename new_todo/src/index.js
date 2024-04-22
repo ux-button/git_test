908c001555f27bc64d90c0006d3e0cd6113ac37f
Хотленко Алексей <khotlenko@www.mts.ru>
2024-04-22 21:14:53 +0300

    Try class communication

--- new_todo/src/index.js
+++ new_todo/src/index.js
@@ -1,1 +1,54 @@
-console.log('Hello!')
+class Task {
+    constructor(taskName) {
+        this.taskName = taskName;
+    }
+
+    get taskName() {
+        return this._taskName;
+    }
+
+    set taskName(value) {
+        this._taskName = value;
+    }
+}
+
+class TasksGroup {
+    constructor(name) {
+        this.name = name;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+}
+
+class TaskStorage {
+    constructor() {
+        this.storage = [];
+    }
+
+    get storage() {
+        return this._storage;
+    }
+
+    set storage(value) {
+        this._storage = value;
+    }
+
+    addTaskToStorage(task) {
+        this.storage.push(task);
+    }
+}
+const oneStorage = new TaskStorage();
+
+const firstTask = new Task('Go play some games!');
+const secondTask = new Task('What next');
+
+oneStorage.addTaskToStorage(secondTask)
+
+console.log(oneStorage)
+oneStorage.storage.map((x) => console.log(x._taskName))
