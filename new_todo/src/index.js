821321be00f9c20da2b4454e976306c03b182b70
Хотленко Алексей <khotlenko@www.mts.ru>
2024-04-24 11:44:37 +0300

    Add default render for To-Do app

--- new_todo/src/index.js
+++ new_todo/src/index.js
@@ -1,54 +1,94 @@
 class Task {
-    constructor(taskName) {
-        this.taskName = taskName;
-    }
-
-    get taskName() {
-        return this._taskName;
-    }
-
-    set taskName(value) {
-        this._taskName = value;
-    }
-}
-
-class TasksGroup {
+    #taskName;
     constructor(name) {
-        this.name = name;
+        this.#taskName = name;
     }
-
-    get name() {
-        return this._name;
+    getTaskName() {
+        return this.#taskName;
     }
-
-    set name(value) {
-        this._name = value;
+    setTaskName(value) {
+        this.#taskName = value;
     }
 }
 
-class TaskStorage {
-    constructor() {
-        this.storage = [];
-    }
 
-    get storage() {
-        return this._storage;
+class Group {
+    #tasksList = [];
+    #taskQuantity = this.#tasksList.length;
+    constructor(groupName) {
+        this.groupName = groupName;
     }
-
-    set storage(value) {
-        this._storage = value;
+    getTasksQuantity() {
+        return this.#taskQuantity;
     }
-
-    addTaskToStorage(task) {
-        this.storage.push(task);
+    getTasksList() {
+        return this.#tasksList;
+    }
+    addTaskToList(task) {
+        this.#tasksList.push(task);
     }
 }
-const oneStorage = new TaskStorage();
 
-const firstTask = new Task('Go play some games!');
-const secondTask = new Task('What next');
 
-oneStorage.addTaskToStorage(secondTask)
+class Render {
+    renderTask(task) { 
+    }
+    renderContainer(parent, type, name, value) {
+        const container = document.createElement(type);
+        container.setAttribute(name, value);
+        document.querySelector(parent).appendChild(container);
+    }
+    renderInput(parent, idName) {
+        const container = document.createElement('input');
+        container.setAttribute('id', idName)
+        container.setAttribute('placeholder', 'What to do?');
+        container.setAttribute('autofocus', '');
+        container.setAttribute('autocomplete', 'off');
+        container.setAttribute('type', 'text');
+        container.setAttribute('name', 'task-name');
+        document.querySelector(parent).appendChild(container);
+    }
+    renderButton(parent, textOnButton, idName) {
+        const container = document.createElement('button');
+        container.setAttribute('id', idName);
+        container.textContent = textOnButton;
+        document.querySelector(parent).appendChild(container);
+    }
+    renderHeader(parent, headerType, headerText, idName) {
+        const container = document.createElement(headerType);
+        container.setAttribute('id', idName);
+        container.textContent = headerText;
+        document.querySelector(parent).appendChild(container);
+    }
+}
 
-console.log(oneStorage)
-oneStorage.storage.map((x) => console.log(x._taskName))
+
+// Create storage
+const storageHandler = (() => {
+    let storage = [];
+    const getStorage = () => {
+        return storage;
+    }
+    const addToStorage = (group) => {
+        storage.push(group)
+    }
+    return { getStorage, addToStorage }
+})();
+
+
+// Default controls and group render
+const domReadyHandler = (() => {
+    // Render default controls
+    const container = new Render();
+    document.addEventListener('DOMContentLoaded', () => {
+        container.renderContainer('body', 'div', 'id', 'new-task-container');
+        container.renderContainer('body', 'div', 'id', 'task-list-container');
+        container.renderInput('#new-task-container', 'task-input');
+        container.renderButton('#new-task-container', 'Add task', 'add-task')
+    })
+    // Render default group
+    const areaGroupName = 'Default';
+    const defaultGroup = new Group(areaGroupName);
+    storageHandler.addToStorage(defaultGroup);
+    container.renderHeader('body', 'h1', areaGroupName, areaGroupName);
+})();
