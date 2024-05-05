062645e6dfb73f3539b53e385a57ebbca4215a91
Хотленко Алексей <khotlenko@www.mts.ru>
2024-05-05 17:21:26 +0300

    Built form validation project

--- validation/validation.js
+++ validation/validation.js
@@ -11,8 +11,24 @@ const confirmInput = document.getElementById('confirmation');
 const confirmError = document.querySelector('#confirmation + div.error');
 
 
+const zipPatterns = {
+    fr: [
+        '^(F-)?\\d{5}$',
+        'France ZIPs must have exactly 5 digits: e.g. F-75012 or 75012'
+    ],
+    de: [
+        '^(D-)?\\d{5}$',
+        'Germany ZIPs must have exactly 5 digits: e.g. D-12345 or 12345'
+    ],
+    nl: [
+        '^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$',
+        'Netherland ZIPs must have exactly 4 digits, followed by 2 letters except SA, SD and SS'
+    ],
+}
+
+
 const checkInput = (() => {
-    const email = (() => {
+    (() => {
         emailInput.addEventListener('input', (event) => {
             if (emailInput.validity.valid) {
                 emailError.textContent = '';
@@ -22,16 +38,73 @@ const checkInput = (() => {
             }
         })
     })();
-    const country = (() => {
-        countryInput.addEventListener('input', (event) => {
-            if (confirmInput.validity.valid) {
-                countryError.textContent = '';
-                countryError.className = 'error';
+    (() => countryInput.addEventListener('change', () => {
+        // Set pattern to ZIP-code field
+        const country = countryInput.value;
+        zipInput.setAttribute('pattern', zipPatterns[country][0]);
+        zipInput.removeAttribute('disabled');
+    }))();
+    (() => zipInput.addEventListener('input', () => {
+        const country = countryInput.value;
+        if (zipInput.validity.valid) {
+            zipError.textContent = '';
+            zipError.className = 'error';
+        } else {
+            showError.zipcode(country);
+        }
+    }))();
+    (() => {
+        passwordInput.addEventListener('input', () => {
+            if (passwordInput.validity.valid) {
+                passwordError.textContent = '';
+                passwordError.className = 'error';
             } else {
-                showError.country();
+                showError.password();
             }
         })
     })();
+    (() => {
+        confirmInput.addEventListener('input', () => {
+            if (passwordInput.value === confirmInput.value) {
+                confirmError.textContent = '';
+                confirmError.className = 'error';
+            } else {
+                confirmError.textContent = 'Passwords should match';
+                confirmError.className = 'error active';
+            }
+        })
+    })();
+})();
+
+
+const checkSubmit = (() => {
+    document.querySelector('button').addEventListener('click', (event) => {
+        // Check email
+        if (emailInput.validity.valid) {
+            emailError.textContent = '';
+            emailError.className = 'error';
+        } else {
+            showError.email();
+        }
+
+        // Check ZIP-code
+        if (zipInput.validity.valid) {
+            zipError.textContent = '';
+            zipError.className = 'error';
+        } else {
+            showError.zipcode(country);
+        }
+
+        // Check password
+        if (passwordInput.validity.valid) {
+            passwordError.textContent = '';
+            passwordError.className = 'error';
+        } else {
+            showError.password();
+        }
+
+        event.preventDefault();
+    })
 })();
 
 
@@ -42,16 +115,35 @@ const showError = (() => {
             emailError.textContent = 'Enter your email';
         } else if (emailInput.validity.typeMismatch) {
             emailError.textContent = 'Enter valid email';
+        // Check valid pattern
         } else if (emailInput.validity.patternMismatch) {
             emailError.textContent = 'Enter email in name@domain.com format'
         }
         emailError.className = 'error active';
     }
-    const country = () => {
-        if (countryInput.validity.valueMissing) {
-            countryError.textContent = 'Enter your country';
+    const zipcode = (country) => {
+        if (zipInput.validity.valueMissing) {
+            zipError.textContent = 'Enter ZIP code';
+            zipError.className = 'error warning';
+        } else if (zipInput.validity.tooShort) {
+            // Check for chosen pattern
+            zipError.textContent = zipPatterns[country][1];
+            zipError.className = 'error warning';
         }
-        countryError.className = 'error active';
+        else if (zipInput.validity.patternMismatch) {
+            // Check for chosen pattern
+            zipError.textContent = zipPatterns[country][1];
+            zipError.className = 'error active';
+        }
     }
-    return { email, country }
+    const password = () => {
+        if (passwordInput.validity.valueMissing) {
+            passwordError.textContent = 'Set password';
+            passwordError.className = 'error warning'
+        } else if (passwordInput.validity.tooShort) {
+            passwordError.textContent = 'Minimum 8 symbols';
+            passwordError.className = 'error warning'
+        }
+    }
+    return { email, zipcode, password }
 })();
