9387b1cb60f6042ba461f7f7bf3e827580d58145
Хотленко Алексей <khotlenko@www.mts.ru>
2024-05-11 21:30:59 +0300

    End weather project

--- weather_api/weather.js
+++ weather_api/weather.js
@@ -3,6 +3,8 @@ const searchInput = document.getElementById('search');
 const temperatureCelcius = document.querySelector('#weather h1');
 const currentCondition = document.querySelector('#weather .text-bold');
 const locationName = document.querySelector('#weather .text-regular');
+const weatherContainer = document.getElementById('weather');
+
 
 const getWeather = async () => {
     const location = searchInput.value;
@@ -13,6 +15,8 @@ const getWeather = async () => {
 const showWeather = async () => {
     try {
         const currentWeather = await getWeather();
+        const temperature = Math.round(currentWeather['current']['temp_c']);
+        colorTemp(temperature, weatherContainer)
         locationName.textContent = currentWeather['location']['region'];
         temperatureCelcius.textContent = `${Math.round(currentWeather['current']['temp_c'])}°`;
         currentCondition.textContent = currentWeather['current']['condition']['text'];
@@ -20,6 +24,30 @@ const showWeather = async () => {
         locationName.textContent = 'No such location';
         temperatureCelcius.textContent = '';
         currentCondition.textContent = '';
+    }
+}
+
+const colorTemp = (temperature, containerName) => {
+    if (temperature < -20) {
+        containerName.className = 'color-01';
+    } else if (temperature < -10) {
+        containerName.className = 'color-02';
+    } else if (temperature < 0) {
+        containerName.className = 'color-03';
+    } else if (temperature < 5) {
+        containerName.className = 'color-04';
+    } else if (temperature < 10) {
+        containerName.className = 'color-05';
+    } else if (temperature < 15) {
+        containerName.className = 'color-06';
+    } else if (temperature < 20) {
+        containerName.className = 'color-07';
+    } else if (temperature < 25) {
+        containerName.className = 'color-08';
+    } else if (temperature < 30) {
+        containerName.className = 'color-09';
+    } else {
+        containerName.className = 'color-10';
     }
 }
 
